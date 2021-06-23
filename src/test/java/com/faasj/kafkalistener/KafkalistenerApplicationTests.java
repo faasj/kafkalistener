35e223fc2ef83c82b642093be74b311af1a63837
Roman Ivanov <roivanoff@gmail.com>
2021-06-23 10:34:57 +0300

    initial

---
+++ src/test/java/com/faasj/kafkalistener/KafkalistenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.faasj.kafkalistener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkalistenerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
